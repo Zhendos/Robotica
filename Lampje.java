c6fe0de99d1bfc8dde81ee8abc06a6019fc42d2d
Zhendos <youri_da_bomb@hotmail.com>
2015-04-17 15:10:30 +0200

    Update Lampje.java

--- Lampje.java
+++ Lampje.java
@@ -16,3 +16,13 @@ void loop()
   }
 
 }
+
+/**
+ * 1) Een draad vanaf de 5volt naar het begin van de regel van breadbord achterin (power)
+ * 2) Een draad van GND naar het breadbord buitenste rij (power)
+ * 3) Een draad van een poort naar het breadbord lange poot.
+ * 4) Een weerstand van de korte poot van het lampje.
+ * 5) Een draad van de andere poot van de weerstand naar de buitenkant, op dezelfde rij waar ook de andere draad naar de GND.
+ * 
+ * 
+ **/
